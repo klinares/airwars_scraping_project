06963292d02440c91ebe23e52b9dc5064555cab2
klinares <kev.a.linares@gmail.com>
2024-11-18 14:31:27 -0500

    adding webpages

--- code/helpful_functions.R
+++ code/helpful_functions.R
@@ -2,14 +2,49 @@
 # ___ Functions for scraping incidents from airwars & process into database ____
 # ______________________________________________________________________________
 
+options(scipen=999)
+
+
+scrape_metadata_fun <- function(website){
+  
+  tibble(
+    # scrape date of incident
+    Incident_Date = html_nodes(
+      website, xpath = 
+        '//*[contains(concat( " ", @class, " " ), concat( " ", "incidentpreview__date", " " ))]//h1') |>  
+      html_text2(),
+    # scrape airwars unique id of incident
+    Incident_id = html_nodes(
+      website, xpath = 
+        
+        '//*[contains(concat( " ", @class, " " ), concat( " ", "meta-block", " " ))]//span') |> 
+      html_text2() 
+  ) |> 
+    # we need to reformat the variables and build out the url
+    mutate(Incident_id = str_to_lower(Incident_id),
+           Incident_Date = str_to_lower(Incident_Date),
+           Incident_Date = str_remove(Incident_Date, ","),
+           Incident_Date = str_replace_all(Incident_Date, " ", "-"),
+           link = glue(
+             "https://airwars.org/civilian-casualties/{Incident_id}-{Incident_Date}/")) 
+  
+}
+
+
+
+
+
+
 pull_assessment_fun <- function(web_content){
   
   
   tibble(
     # attach incident id number
     Incident_id = web_content[[1]],
-    assessment = html_nodes(web_content[[2]], xpath = 
-                              '//*[contains(concat( " ", @class, " " ), concat( " ", "summary", " " ))]//p') |> 
+    assessment = html_nodes(
+      web_content[[2]], 
+      xpath = 
+        '//*[contains(concat( " ", @class, " " ), concat( " ", "summary", " " ))]//p') |> 
       html_text2() |> toString()
   ) 
 }
@@ -36,40 +71,94 @@ sentiment_score_fun <- function(incident_assessment){
 
 
 
+attack_location_fun <- function(incident_assessment){
+  
+  description = incident_assessment
+  
+  # use huggingface model to detect emotional tone of summary
+  mod = 
+    textQA(question = "Where was the location of the attack?",
+           context = description,
+                 model = "deepset/roberta-base-squad2", 
+                 device = "gpu")$answer
+}
 
+
+
+
+# reverse coordinates
 pull_coords_fun <- function(web_content){
   
+  geo_coords = html_nodes(
+    web_content[[2]], 
+    xpath = 
+      '//*[contains(concat( " ", @class, " " ), concat( " ", "lat-lng", " " ))]')
   
-  
-  if(web_content[[2]] |> html_nodes(".geolocation-notes") |> length() == 0 ){
-    tibble(lat=NA, long=NA, type_location = NA)
-    
+  if(length(geo_coords) > 0){
+    dat = geo_coords |> 
+      html_text2() |>
+      as_tibble() |> 
+      separate(value, into=c("incident_lat", "incident_long"), sep=", ") |> 
+      slice(1)
+
   } else{
-    web_content[[2]] |> html_nodes(".geolocation-notes") |> html_text2() |>
-      str_extract_all("\\d+\\.\\d+") |>
-      unlist() |>
-      bind_cols() |>
-      data.table::transpose() |>
-      rename(lat=1, long=2) |> 
-      # locate the type of location using coordinates
-      mutate(lat = as.numeric(lat),
-             long = as.numeric(long),
-             type_location = map2(lat, long, function(x, y){
-               
-               # hit the OSM API
-               fromJSON(
-                 glue(
-                   "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={x}&lon={y}")
-               )$type
-             }),
-             type_location = as.character(type_location))
-  } |> 
-    mutate(across(where(is.list), ~ as.character(.x)),
-           # attach incident id number
-           Incident_id = web_content[[1]]) |> 
-    select(Incident_id, lat, long, type_location) |> 
-    drop_na()
+    dat = tibble(incident_lat=NA, incident_long=NA)
+  }
+  
+  return(dat)
 }
+
+
+
+
+# find location from text search 
+find_location_coord_fun <- function(location_found){
+  
+  # clean the string before creating URL
+  location_attack = str_replace_all(location_found, "’", "%60")
+  location_attack = str_remove_all(location_found, "\\(|Education|Kinda|\\)")
+  location_attack = str_replace_all(location_found, " ", "+")
+  
+  
+  nominatim_request = tryCatch(
+    fromJSON(
+      glue(
+        "https://nominatim.openstreetmap.org/search?addressdetails=1&q={location_attack}&format=jsonv2&limit=1")
+      ), 
+    error = function(e) {return(NA)} )
+  
+  if(length(nominatim_request) < 2){
+    
+    dat = tibble(lat = NA, long = NA, 
+                 lat_min=NA, lat_max=NA, long_min=NA, long_max=NA)
+  } else{
+      
+      dat = tibble(
+        lat = nominatim_request$lat[[1]],
+        long = nominatim_request$lon[[1]],
+      ) |> 
+        add_column(
+          nominatim_request$boundingbox[[1]] |> 
+            as_tibble() |> 
+            data.table::transpose() |> 
+            rename(lat_min=1, lat_max=2, long_min=3, long_max=4)
+        )
+  } 
+  
+  # convert bbox diagonal distance
+  dat = dat |> 
+    # use geosphere to calculate distance in meters
+    mutate(lat_min = as.numeric(lat_min),
+           lat_max = as.numeric(lat_max),
+           long_min = as.numeric(long_min),
+           long_max = as.numeric(long_max),
+           bbox_dist = as.vector(distm(
+             c(long_min, lat_min), c(long_max, lat_max), 
+             fun = distHaversine)) )
+  
+  return(dat)
+}
+
 
 
 
@@ -93,24 +182,33 @@ pull_incident_fun <- function(web_content){
     select(-Incident_notes, -var_with_casualty_type)|> 
     pivot_wider(names_from = Incident_field, values_from = Incident_result) |> 
     mutate(across(where(is.list), ~ as.character(.x)),
-           # attach incident id number
-           Incident_id = web_content[[1]]) |> 
-    relocate(Incident_id) |> 
-    mutate(civilians_reported_killed = 
+           civilians_reported_killed = 
              na_if(`Civilians reported killed`, "Unknown"),
            casualty_estimate = ifelse(str_detect(
              civilians_reported_killed, "–"), "range", "absolute") 
-    ) |> 
+           ) |> 
     separate(civilians_reported_killed, c("min_killed", "max_killed")) |> 
     mutate(max_killed = ifelse(is.na(max_killed), min_killed, max_killed),
            # combine kill ranges
            min_killed = as.integer(min_killed), 
            max_killed = as.integer(max_killed)) |> 
     mutate(killed = (min_killed + max_killed) / 2,
-           killed = as.integer(killed)) 
+            killed = as.integer(killed)) |> 
+    # # add location metadata
+    mutate(location_meta =
+             html_nodes(web_content[[2]],
+                        xpath=
+                          '//*[contains(concat( " ", @class, " " ), concat( " ", "location", " " ))]') |>
+             html_text2() |>
+             # remove Arabic characters, keep English translation
+             str_replace_all( "[^a-zA-Z0-9]|Location", " ") |>
+             str_trim()
+            )
   
   return(dat)
 }
+
+
 
 
 #____________________________ END _________________________________
--- code/scrape_process_incidence.R
+++ code/scrape_process_incidence.R
@@ -4,7 +4,7 @@
 
 source("~/repos/airwars_scraping_project/code/helpful_functions.R")
 
-pacman::p_load(rvest, lubridate, RSQLite, DBI, text,
+pacman::p_load(rvest, lubridate, RSQLite, DBI, text, geosphere,
                glue, jsonlite, tidyverse, data.table)
 
 
@@ -14,27 +14,7 @@ pacman::p_load(rvest, lubridate, RSQLite, DBI, text,
 airwars_website <- read_html("https://airwars.org/conflict/israel-and-gaza-2023/")
 
 # store needed information, and process
-airwars_meta <- tibble(
-  # scrape date of incident
-  Incident_Date = html_nodes(
-    airwars_website, xpath = 
-      '//*[contains(concat( " ", @class, " " ), concat( " ", "incidentpreview__date", " " ))]//h1') |>  
-    html_text2(),
-  # scrape airwars unique id of incident
-  Incident_id = html_nodes(
-    airwars_website, xpath = 
-                             
-      '//*[contains(concat( " ", @class, " " ), concat( " ", "meta-block", " " ))]//span') |> 
-    html_text2() 
-) |> 
-  # we need to reformat the variables and build out the url
-  mutate(Incident_id = str_to_lower(Incident_id),
-         Incident_Date = str_to_lower(Incident_Date),
-         Incident_Date = str_remove(Incident_Date, ","),
-         Incident_Date = str_replace_all(Incident_Date, " ", "-"),
-         link = glue(
-           "https://airwars.org/civilian-casualties/{Incident_id}-{Incident_Date}/")) 
-
+airwars_meta <- scrape_metadata_fun(airwars_website)
 
 # convert each row, incident, into a list
 airwars_meta_list <- airwars_meta |> 
@@ -42,14 +22,17 @@ airwars_meta_list <- airwars_meta |>
   group_by(id) |> 
   group_split()
 
+# change date string to date variable
+airwars_meta <- airwars_meta |> 
+  mutate(Incident_Date = mdy(Incident_Date))
 
 
 #____________________________ END _________________________________
-
+ 
 
 # ________________________ Read in incident URLs ________________________
 
-# save each web page in a list
+# 1. save each web page in a list
 incident_content <- map(airwars_meta_list, function(x){ 
   
   url_content = list(Incident_id = x[[2]],
@@ -57,31 +40,33 @@ incident_content <- map(airwars_meta_list, function(x){
   )
 })
 
+# write webpages out to repo
+folder_path <- "~/repos/airwars_scraping_project/database/webpages/"
 
-# scrape needed content from the web papers & build tables
+map(incident_content, function(x){
+  
+  file_name = str_c(x[[1]], ".html")
+
+ xml2::write_html(x[[2]], str_c(folder_path, file_name))
+})
+
+
+
+
+
+# 2. scrape needed content from the web papers & build tables
 
 ## scrape incident assessment
-airwars_assessment <- map_dfr(incident_content, function(x){ 
+airwars_incidents <- map_dfr(incident_content, function(x){ 
   # parse out summary of event
   pull_assessment_fun(x) |> 
     as_tibble() |> 
-    mutate(sentiment_score_fun(assessment))
-}) 
-
-
-
-# scrape geolocation  data
-## find location type when coordinates are present
-airwars_coord <- map_dfr(incident_content, function(x){ 
-  # parse out summary of event
-  pull_coords_fun(x)
-}) 
-
-
-
-airwars_incidents <- map_dfr(incident_content, function(x){ 
-  # parse out summary of event
-  pull_incident_fun(x)
+    mutate(sentiment_score_fun(assessment),
+           attack_location = attack_location_fun(assessment),
+           pull_coords_fun(x),
+           find_location_coord_fun(attack_location),
+           pull_incident_fun(x) 
+    )
 }) |> 
   # we need to extract number killed by men, women, children
   mutate(children_killed = as.integer(
@@ -92,7 +77,58 @@ airwars_incidents <- map_dfr(incident_content, function(x){
       str_extract(Civilian_type, "(\\d)+(?= m)"))) |>
   relocate(Civilian_type, .after=last_col())
 
+
+
+
+
+# 3. process casualty daily deaths from https://data.techforpalestine.org/docs/casualties-daily/
+daily_casualties <- read_csv(
+  "~/repos/airwars_scraping_project/raw_data/casualties_daily_gaza.csv") |> 
+  select(
+    report_date, ext_killed, ext_killed_cum, 
+    ext_killed_children_cum, ext_killed_women_cum) |> 
+  # we need to create a lag count for children and women
+  mutate(ext_killed_children = 
+           ext_killed_children_cum - lag(ext_killed_children_cum),
+         ext_killed_women = 
+           ext_killed_women_cum - lag(ext_killed_women_cum)) |> 
+  # convert first rows to 0.
+  mutate_at(c(6:7), ~replace_na(., 0)) |> 
+  rename(Incident_Date = report_date)
+
+
+# process Gaza conflict data from https://acleddata.com/data-export-tool/
+daily_conflict <- read_csv(
+  "~/repos/airwars_scraping_project/raw_data/2023-10-02-2024-11-01-Palestine_acled.csv") |> 
+  mutate(event_date = dmy(event_date)) |> 
+  rename(Incident_Date = event_date, 
+         lat = latitude,
+         long=longitude) |> 
+  arrange(Incident_Date) |> 
+  # we want to use the OSM API to get boundary boxes of targeted places
+  mutate(map2_dfr(lat, long, function(x, y){
+    
+    # hit the OSM API
+    osm_coord = fromJSON(
+      glue(
+        "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={x}&lon={y}")
+    )
+    # skip coordinates that do not resolve
+    if(osm_coord[[1]] == "Unable to geocode") {
+      tibble(xmin=NA, xmax=NA, ymin=NA, ymax=NA)
+    } else {
+      # keep coordinates that resolve
+      osm_coord$boundingbox |> 
+        as_tibble() |> 
+        data.table::transpose() |> 
+        rename(xmin=1, xmax=2, ymin=3, ymax=4) 
+    }
+  })
+  ) 
+
+
 #____________________________ END _________________________________
+
 
 
 # ___________________ # write results to database table  _______________________
@@ -104,6 +140,9 @@ dbWriteTable(mydb, "airwars_meta", airwars_meta, overwrite=TRUE)
 dbWriteTable(mydb, "airwars_assessment", airwars_assessment, overwrite=TRUE)
 dbWriteTable(mydb, "airwars_coord", airwars_coord, overwrite=TRUE)
 dbWriteTable(mydb, "airwars_incidents", airwars_incidents, overwrite=TRUE)
+dbWriteTable(mydb, "daily_casualties", daily_casualties, overwrite=TRUE)
+dbWriteTable(mydb, "daily_conflict", daily_conflict, overwrite=TRUE)
+
 
 # print tables in database
 dbListTables(mydb)
